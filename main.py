cd9fdc629021e627ec7cb463d82eac7dbb8ddb21
kolix2006 <kolyanychkolya5@gmail.com>
2026-01-30 14:08:13 +0300

    full complete

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # step 1
 
-def get_sum(a, b):
+def get_sum(a: int, b: int) -> int:
     return a + b
 
 # step 2
 
-def count_capital_letters(text):
+def count_capital_letters(text: str) -> int:
     count = 0
     capital_letters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
     for char in text:
@@ -15,7 +15,15 @@ def count_capital_letters(text):
 
 # step 3
 
-
+def decode_string(str):
+    lower_str = str.lower()
+    result = ''
+    for char in str:
+        if lower_str.count(char.lower()) == 1:
+            result += '('
+        else:
+            result += ')'
+    return result
 
 # step 4
 
@@ -28,3 +36,12 @@ def get_odd_count(num: str) -> int:
 
 # step 5
 
+def check_access(has_keycard: bool, has_fingerprint: bool, is_alarm: bool, is_daylight: bool) -> bool:
+    if is_alarm == True:
+        return False
+    elif has_keycard == True and is_daylight == False:
+        return False
+    elif has_keycard == False and has_fingerprint == False:
+        return False
+    else:
+        return True
--- testin.py
+++ testin.py
@@ -1,3 +1,26 @@
-from main import count_capital_letters
+from main import *
 
-print(main.count_capital_letters('Hello'))
+# 1 step check
+
+print(get_sum(4, 7)) # 11
+
+# 2 step check
+
+print(count_capital_letters('Kim Phing Thao')) # 3
+
+# 3 step check
+
+print(decode_string('Success')) # )())())
+
+# 4 step check
+
+print(get_odd_count('24445')) # 4
+
+# 5 step check
+
+print("Тест 1 (Ключ, день, нет тревоги):", check_access(True, False, False, True))   # Тест 1 (Ключ, день, нет тревоги): True
+print("Тест 2 (Ключ, ночь, нет тревоги):", check_access(True, False, False, False))  # Тест 2 (Ключ, ночь, нет тревоги): False
+print("Тест 3 (Палец, ночь, нет тревоги):", check_access(False, True, False, False)) # Тест 3 (Палец, ночь, нет тревоги): True
+print("Тест 4 (Палец, день, но ЕСТЬ тревога):", check_access(False, True, True, True)) # Тест 4 (Палец, день, но ЕСТЬ тревога): False
+print("Тест 5 (Ключ И палец, день, нет тревоги):", check_access(True, True, False, True)) # Тест 5 (Ключ И палец, день, нет тревоги): True
+print("Тест 6 (Ничего нет):", check_access(False, False, False, True))               # Тест 6 (Ничего нет): False
